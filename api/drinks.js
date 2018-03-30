7bd45577d88027d5f2054be69a32e0ac2b1a4e8c
Dan Rabinovitch <danrab@amazon.com>
2018-03-30 14:04:11 -0700

    initial api

---
+++ api/drinks.js
@@ -0,0 +1,28 @@
+'use strict';
+let drinks = require('./data')
+
+exports.handler = (event, context, callback) => {
+    let id = event.pathParameters ? event.pathParameters.id-1 : null
+    switch(event.httpMethod){
+        
+        case "GET":
+            if(id != null) {
+                callback(null, {
+                   statusCode: 200,
+                   body: JSON.stringify(drinks[id])
+                })
+                return
+            } 
+            
+            callback(null, { 
+               statusCode: 200,
+               body: JSON.stringify(drinks)
+            });
+            break
+            
+        default:
+            // Send HTTP 501: Not Implemented
+            console.log("Error: unsupported HTTP method (" + event.httpMethod + ")")
+            callback(null, { statusCode: 501 })
+    }
+}
