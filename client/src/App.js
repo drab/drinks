b33b436bb37fd1db4b333b034e25bf1e19e1d8c9
Dan Rabinovitch <danrab@amazon.com>
2018-04-05 16:11:50 -0700

    format with Prettier

--- client/src/App.js
+++ client/src/App.js
@@ -3,70 +3,70 @@ import { Container, Header, Segment, Button, Icon, Dimmer, Loader, Divider } fro
 import config from './config'
 
 class App extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {}
     this.getDrinks = this.getDrinks.bind(this)
     this.getDrink = this.getDrink.bind(this)
   }
-  componentDidMount () {
+  componentDidMount() {
     this.getDrinks()
   }
-  fetch (endpoint) {
+  fetch(endpoint) {
     return new Promise((resolve, reject) => {
       window.fetch(config.apiGateway.URL + endpoint)
-      .then(response => response.json())
-      .then(json => resolve(json))
-      .catch(error => reject(error))
+        .then(response => response.json())
+        .then(json => resolve(json))
+        .catch(error => reject(error))
     })
   }
-  getDrinks () {
+  getDrinks() {
     this.fetch('api/drinks')
       .then(drinks => {
-        this.setState({drinks: drinks})
+        this.setState({ drinks: drinks })
         this.getDrink(drinks[0].id)
       })
   }
-  getDrink (id) {
+  getDrink(id) {
     this.fetch(`api/drinks/${id}`)
-      .then(drink => this.setState({drink: drink}))
+      .then(drink => this.setState({ drink: drink }))
   }
-  render () {
-    let {drinks, drink} = this.state
+  render() {
+    let { drinks, drink } = this.state
     return drinks
-    ? <Container text>
+      ? <Container text>
         <Header as='h2' icon textAlign='center'>
-        <Icon name='cocktail' circular />
-        <Header.Content>
-          List of Ingredients
+          <Icon name='cocktail' circular />
+          <Header.Content>
+            List of Ingredients
         </Header.Content>
-      </Header>
-      <Button.Group fluid widths={drinks.length}>
-        {Object.keys(drinks).map((key) => {
-          return <Button active={drink && drink.id === drinks[key].id} fluid key={key} onClick={() => this.getDrink(drinks[key].id)}>
-            {drinks[key].title}
-          </Button>
-        })}
-      </Button.Group>
-      <Divider hidden />
-      {drink &&
-        <Container>
-          <Header as='h2'>{drink.title}</Header>
-          {drink.description && <p>{drink.description}</p>}
-          {drink.ingredients &&
-            <Segment.Group>
-              {drink.ingredients.map((ingredient, i) => <Segment key={i}>{ingredient.description}</Segment>)}
-            </Segment.Group>
-          }
-          {drink.steps && <p>{drink.steps}</p>}
-        </Container>
-      }
-    </Container>
-    : <Container text>
-      <Dimmer active inverted>
-        <Loader content='Loading' />
-      </Dimmer>
-    </Container>
+        </Header>
+        <Button.Group fluid widths={drinks.length}>
+          {Object.keys(drinks).map((key) => {
+            return <Button active={drink && drink.id === drinks[key].id} fluid key={key} onClick={() => this.getDrink(drinks[key].id)}>
+              {drinks[key].title}
+            </Button>
+          })}
+        </Button.Group>
+        <Divider hidden />
+        {drink &&
+          <Container>
+            <Header as='h2'>{drink.title}</Header>
+            {drink.description && <p>{drink.description}</p>}
+            {drink.ingredients &&
+              <Segment.Group>
+                {drink.ingredients.map((ingredient, i) => <Segment key={i}>{ingredient.description}</Segment>)}
+              </Segment.Group>
+            }
+            {drink.steps && <p>{drink.steps}</p>}
+          </Container>
+        }
+      </Container>
+      : <Container text>
+        <Dimmer active inverted>
+          <Loader content='Loading' />
+        </Dimmer>
+      </Container>
   }
 }
 
