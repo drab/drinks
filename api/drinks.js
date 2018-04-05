fe149b494727178f1133ee617bff050d05b57a44
Dan Rabinovitch <danrab@amazon.com>
2018-04-05 16:10:54 -0700

    format with Prettier

--- api/drinks.js
+++ api/drinks.js
@@ -2,32 +2,32 @@
 let drinks = require('./data')
 
 exports.handler = (event, context, callback) => {
-    let id = event.pathParameters ? event.pathParameters.id-1 : null
-    switch(event.httpMethod){
-        
+    let id = event.pathParameters ? event.pathParameters.id - 1 : null
+    switch (event.httpMethod) {
+
         case "GET":
-            if(id != null) {
+            if (id != null) {
                 callback(null, {
-                   statusCode: 200,
-                   headers: {
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Credentials": "true"
-                   },
-                   body: JSON.stringify(drinks[id])
+                    statusCode: 200,
+                    headers: {
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Credentials": "true"
+                    },
+                    body: JSON.stringify(drinks[id])
                 })
                 return
-            } 
-            
-            callback(null, { 
-               statusCode: 200,
-               headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Credentials": "true"
-               },
-               body: JSON.stringify(drinks)
+            }
+
+            callback(null, {
+                statusCode: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Credentials": "true"
+                },
+                body: JSON.stringify(drinks)
             });
             break
-            
+
         default:
             // Send HTTP 501: Not Implemented
             console.log("Error: unsupported HTTP method (" + event.httpMethod + ")")
